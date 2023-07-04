2f42bd8c8e580759e94dd0f32c6eec4746465fa4
Diky Nawa Dwi Putra <96540094+DikyNawa@users.noreply.github.com>
2023-07-04 19:14:54 +0700

    Update 02_🔮_Model.py

--- pages/02_🔮_Model.py
+++ pages/02_🔮_Model.py
@@ -307,7 +307,7 @@ else:
         df_linechart = data_awal[data_awal['Provinsi'] == provinsi]
         df_linechart = df_linechart[['Provinsi' ,'Tahun', 'Produksi']]
 
-        df_linechart.loc[len(df_linechart)] = [provinsi, tahun, result.values]
+        df_linechart.loc[len(df_linechart)] = [provinsi, tahun, result[0]]
 
         df_res_linechart = pd.DataFrame({'Provinsi':[provinsi],
                                          'Tahun':[tahun],
